1151f30013c746e4d32e66bd20872e6622c3fa84
Robert Vari <mail.robertvari@gmail.com>
2024-02-03 17:42:14 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -7,9 +7,9 @@ class Game:
         self.computer = Computer()
         self.player = Player()
 
-
     def start(self):
         self.intro()
+        self.game_loop()
 
     def intro(self):
         print("*"*50, "MAGIC NUMBER GAME", "*"*50)
@@ -23,15 +23,27 @@ class Game:
     def clear_screen(self):
         os.system("cls")
 
+    def game_loop(self):
+        self.computer.think_a_number()
+        self.player.think_a_number()
+
 class Computer:
     def __init__(self):
         self.max_number = 10
-        self.magic_number = 0
+        self.my_number = "0"
+        self.max_tries = 3
+    
+    def think_a_number(self):
+        self.my_number = str(random.randint(1, self.max_number))
 
 class Player:
     def __init__(self):
         self.name = "Robert"
         self.credits = 100
+        self.my_number = "0"
+    
+    def think_a_number(self):
+        self.my_number = input("What is your guess?")
 
     def __str__(self) -> str:
         return self.name
