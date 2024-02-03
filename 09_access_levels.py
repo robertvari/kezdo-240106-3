ce99424156cce9650698e76efeb576da5440e9ed
Robert Vari <mail.robertvari@gmail.com>
2024-02-03 16:29:48 +0100

    update

--- 09_access_levels.py
+++ 09_access_levels.py
@@ -5,18 +5,24 @@ class Dice:
         # public attributes
         self.sides = sides
         self.color = color
+
+        # protected attributes
+        self._sides = sides
+        self._color = color
+
+        # private attributes
+        self.__sides = sides
+        self.__color = color
     
     def __str__(self) -> str:
-        return f"Sides: {self.sides} Color: {self.color}"
+        return f"Sides: {self.__sides} Color: {self.__color}"
     
     def roll(self):
-        print(f"D{self.sides} is rolling...")
+        print(f"D{self.__sides} is rolling...")
         time.sleep(2)
-        print(f"D{self.sides} result: {random.randint(1, self.sides)}")
+        print(f"D{self.__sides} result: {random.randint(1, self.__sides)}")
 
 d6 = Dice(6, "white")
 d20 = Dice(20, "blue")
-
-d6.sides = 10
 
 d6.roll()
