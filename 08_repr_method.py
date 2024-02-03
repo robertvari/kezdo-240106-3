f7b342b4852875052dd884f5c66102a62f9741e3
Robert Vari <mail.robertvari@gmail.com>
2024-02-03 15:49:39 +0100

    update

---
+++ 08_repr_method.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def __str__(self):
+        return f"Name: {self.name} Age: {self.age} Address: {self.address}"
+    
+    def __repr__(self):
+        return self.name
+
+csaba = Person("Csaba", 22, "Pécs")
+kriszta = Person("Kriszta", 25, "Bodapest")
+balazs = Person("Balázs", 20, "Debrecen")
+
+print(csaba)
+
+my_friends = [csaba, kriszta, balazs]
+print(my_friends)
