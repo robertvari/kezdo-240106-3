84a1d8927b7616b4af0c4a20e4daf6bc5c416021
Robert Vari <mail.robertvari@gmail.com>
2024-02-04 17:11:44 +0100

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -3,32 +3,34 @@ import random
 # Base class of a Player
 class PlayerBASE:
     def __init__(self):
-        self.__name = None
-        self.__credits = 0
+        self._name = None
+        self._credits = 0
         self.__hand = []
         self.__playing = True
 
-        self.__create()
+        self._create()
 
-    def __create(self):
-        self.__credits = random.randint(10, 100)
-        self.__name = self.__get_random_name()
+    def _create(self):
+        self._credits = random.randint(10, 100)
+        self._name = self.__get_random_name()
 
     def __get_random_name(self) -> str:
         first_names = ["Marnie", "Johnathan", "Mahnoor", "Hassan", "Alissa", "Millie", "Qasim", "Damon", "Shreya", "Carly"]
         last_names =  ["Roy", "Aguirre", "Sandoval", "Rogers", "Cole", "Matthams", "Allen", "Stokes", "Deleon", "Hampton"]
-        return "Random Name"
+        return f"{random.choice(first_names)} {random.choice(last_names)}"
 
     def __str__(self) -> str:
-        return str(self.__name)
+        return str(self._name)
 
 
 class AIPlayer(PlayerBASE):
     pass
 
 class Player(PlayerBASE):
-    pass
-
+    # we override PlayerBASE._create
+    def _create(self):
+        super()._create()
+        self._name = input("What is your name?")
 
 
 if __name__ == "__main__":
