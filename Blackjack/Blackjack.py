8f38e6f3fe23a5aca162a88adc5ae90f425d269b
Robert Vari <mail.robertvari@gmail.com>
2024-02-04 14:38:28 +0100

    update

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -6,13 +6,23 @@ class Blackjack:
         self.__min_bet = 10
         self.__player_list = []
 
+        # todo create a deck of cards
+        self.__deck = None
+
+        # todo create an AI Player
+        self.__ai_player = None
+
+        # todo create a Player
+        self.__player = None
+
         self.__clear_screen()
         self.__intro()
 
-        # todo create a deck of cards
-        # todo create an AI Player
-        # todo create a Player
         # todo start first turn
+        self.__game_loop()
+
+    def __game_loop(self):
+        pass
 
     # private method
     def __intro(self):
