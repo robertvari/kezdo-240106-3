935a747689b471d9ff48efe9662aacb45141992f
Robert Vari <mail.robertvari@gmail.com>
2024-02-04 14:34:20 +0100

    update

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,9 +1,24 @@
+import os
+
 class Blackjack:
     def __init__(self):
+        self.__reward = 0
+        self.__min_bet = 10
+        self.__player_list = []
+
+        self.__clear_screen()
         self.__intro()
+
+        # todo create a deck of cards
+        # todo create an AI Player
+        # todo create a Player
+        # todo start first turn
 
     # private method
     def __intro(self):
-        print("Blackjack started")
+        print("="*50, "BLACKJACK", "="*50)
+
+    def __clear_screen(self):
+        os.system("cls")
 
 Blackjack()
